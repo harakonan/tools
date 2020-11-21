2449aa3641e2d4b751d55741e07f2849779dea3d
harakonan <hara.konan@gmail.com>
2020-11-21 12:22:17 +0900

    [UPDATE] set_emp_na.R and set_na_0.R

--- r/set_emp_na.R
+++ r/set_emp_na.R
@@ -5,7 +5,7 @@
 # cols can be left unspecified if the target is the entire data.table
 # Requirement:
 # library(data.table)
-set_emp_na <- function(DT, cols = NA) {
+set_emp_na <- function(DT, cols = NULL) {
 	if (length(cols) == 0){
 		for (j in seq_len(ncol(DT))){
 			set(DT,which(DT[[j]] == ""),j,NA)
--- r/set_na_0.R
+++ r/set_na_0.R
@@ -5,7 +5,7 @@
 # cols can be left unspecified if the target is the entire data.table
 # Requirement:
 # library(data.table)
-set_na_0 <- function(DT, cols = NA) {
+set_na_0 <- function(DT, cols = NULL) {
 	if (length(cols) == 0){
 		for (j in seq_len(ncol(DT))){
 			set(DT,which(is.na(DT[[j]])),j,0)
