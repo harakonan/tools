eabd5eb95a427a9b6088cd4dd0023eb2ed6340d2
harakonan <hara.konan@gmail.com>
2020-11-21 11:01:59 +0900

    [UPDATE] manuscript_template.tex and set_na_0.R

--- r/set_na_0.R
+++ r/set_na_0.R
@@ -1,9 +1,18 @@
-# set_na_0: Function that fill NA with 0 for entire data.table
+# set_na_0: Function that fill NA with 0 for the entire or selected columns of a data.table
 # Inputs:
 # DT, data.table
+# cols, selected column names
+# cols can be left unspecified if the target is the entire data.table
 # Requirement:
 # library(data.table)
-set_na_0 <- function(DT) {
-  for (j in seq_len(ncol(DT)))
-    set(DT,which(is.na(DT[[j]])),j,0)
+set_na_0 <- function(DT, cols = NA) {
+	if (length(cols) == 0){
+		for (j in seq_len(ncol(DT))){
+			set(DT,which(is.na(DT[[j]])),j,0)
+		}
+	} else {
+		for (col in cols){
+			set(DT,which(is.na(DT[[col]])),col,0)
+		}
+	}
 }
