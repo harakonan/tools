2da3bc46a0149efde1887d90fe5c02b2f1f4e01b
harakonan <hara.konan@gmail.com>
2021-04-07 17:34:20 -0700

    [UPDATE] r-rnw

--- sh/r-rnw.sh
+++ sh/r-rnw.sh
@@ -1,8 +1,8 @@
 
 #!/bin/bash
 
-sed -i 's/\#\ \\/\\/g' $1.Rnw
-sed -i 's/\#\ <</\<\</g' $1.Rnw
-sed -i 's/\#\ @/@/g' $1.Rnw
+sed -i '' 's/\#\ \\/\\/g' $1.Rnw
+sed -i '' 's/\#\ <</\<\</g' $1.Rnw
+sed -i '' 's/\#\ @/@/g' $1.Rnw
 
 open -a RStudio.app $1.Rnw
