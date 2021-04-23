ffac44c83f1c7d4f3cb1fb99e90adc436bd6f602
harakonan <hara.konan@gmail.com>
2021-04-22 18:45:16 -0700

    [ADD] r/set_nan_na.R

--- r/set_nan_na.R
+++ r/set_nan_na.R
@@ -1,18 +1,18 @@
-# set_na_0: Function that replace NA with 0 for the entire or selected columns of a data.table
+# set_nan_na: Function that replace NaN with NA for the entire or selected columns of a data.table
 # Inputs:
 # DT, data.table
 # cols, selected column names
 # cols can be left unspecified if the target is the entire data.table
 # Requirement:
 # library(data.table)
-set_na_0 <- function(DT, cols = NULL) {
+set_nan_na <- function(DT, cols = NULL) {
 	if (length(cols) == 0){
 		for (j in seq_len(ncol(DT))){
-			set(DT,which(is.na(DT[[j]])),j,0)
+			set(DT,which(is.nan(DT[[j]])),j,NA)
 		}
 	} else {
 		for (col in cols){
-			set(DT,which(is.na(DT[[col]])),col,0)
+			set(DT,which(is.nan(DT[[col]])),col,NA)
 		}
 	}
 }
